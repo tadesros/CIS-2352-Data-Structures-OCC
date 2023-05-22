c0464718890027df4d95c42d11b922029e3da98a
Tom <tomadesrosiers@gmail.com>
2023-05-21 20:50:28 -0400

    afd
    asdf

---
+++ ADTBat/src/adtbag/ADTBag.java
@@ -0,0 +1,10 @@
+package adtbag;
+
+public class ADTBag {
+
+    public static class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+        }
+    }
+}
---
+++ Week2_ADT_Bag/src/adtbag/ADTBag.java
@@ -0,0 +1,2 @@
+package adtbag;public class ADTBag {
+}
